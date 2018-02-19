15889f7dba79945146fc2c779fea31632fb0eb54
FabriGinseng <antoniofabriziofiume@libero.it>
2018-02-19 16:53:30 +0100

    colori modificati piani formativi

--- QuizAmoroso/QuizAmoroso.Android/Resources/Resource.Designer.cs
+++ QuizAmoroso/QuizAmoroso.Android/Resources/Resource.Designer.cs
@@ -4447,26 +4447,26 @@ namespace QuizAmoroso.Droid
 			// aapt resource value: 0x7f020057
 			public const int avd_hide_password = 2130837591;
 			
-			// aapt resource value: 0x7f020154
-			public const int avd_hide_password_1 = 2130837844;
-			
 			// aapt resource value: 0x7f020155
-			public const int avd_hide_password_2 = 2130837845;
+			public const int avd_hide_password_1 = 2130837845;
 			
 			// aapt resource value: 0x7f020156
-			public const int avd_hide_password_3 = 2130837846;
+			public const int avd_hide_password_2 = 2130837846;
+			
+			// aapt resource value: 0x7f020157
+			public const int avd_hide_password_3 = 2130837847;
 			
 			// aapt resource value: 0x7f020058
 			public const int avd_show_password = 2130837592;
 			
-			// aapt resource value: 0x7f020157
-			public const int avd_show_password_1 = 2130837847;
-			
 			// aapt resource value: 0x7f020158
-			public const int avd_show_password_2 = 2130837848;
+			public const int avd_show_password_1 = 2130837848;
 			
 			// aapt resource value: 0x7f020159
-			public const int avd_show_password_3 = 2130837849;
+			public const int avd_show_password_2 = 2130837849;
+			
+			// aapt resource value: 0x7f02015a
+			public const int avd_show_password_3 = 2130837850;
 			
 			// aapt resource value: 0x7f020059
 			public const int btn_Pause = 2130837593;
@@ -5182,11 +5182,11 @@ namespace QuizAmoroso.Droid
 			// aapt resource value: 0x7f020146
 			public const int notification_icon_background = 2130837830;
 			
-			// aapt resource value: 0x7f020152
-			public const int notification_template_icon_bg = 2130837842;
-			
 			// aapt resource value: 0x7f020153
-			public const int notification_template_icon_low_bg = 2130837843;
+			public const int notification_template_icon_bg = 2130837843;
+			
+			// aapt resource value: 0x7f020154
+			public const int notification_template_icon_low_bg = 2130837844;
 			
 			// aapt resource value: 0x7f020147
 			public const int notification_tile_bg = 2130837831;
@@ -5201,25 +5201,28 @@ namespace QuizAmoroso.Droid
 			public const int right = 2130837834;
 			
 			// aapt resource value: 0x7f02014b
-			public const int screen = 2130837835;
+			public const int rightArrow = 2130837835;
 			
 			// aapt resource value: 0x7f02014c
-			public const int shwpssw = 2130837836;
+			public const int screen = 2130837836;
 			
 			// aapt resource value: 0x7f02014d
-			public const int shwpsswblue = 2130837837;
+			public const int shwpssw = 2130837837;
 			
 			// aapt resource value: 0x7f02014e
-			public const int shwpsswblueSegnato = 2130837838;
+			public const int shwpsswblue = 2130837838;
 			
 			// aapt resource value: 0x7f02014f
-			public const int sitoWeb = 2130837839;
+			public const int shwpsswblueSegnato = 2130837839;
 			
 			// aapt resource value: 0x7f020150
-			public const int splash_screen = 2130837840;
+			public const int sitoWeb = 2130837840;
 			
 			// aapt resource value: 0x7f020151
-			public const int telefonoFisso = 2130837841;
+			public const int splash_screen = 2130837841;
+			
+			// aapt resource value: 0x7f020152
+			public const int telefonoFisso = 2130837842;
 			
 			static Drawable()
 			{
