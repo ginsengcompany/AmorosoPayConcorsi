dd6caf0a9e54bd21f9a3e0e3013e3f51f964adc6
FabriGinseng <antoniofabriziofiume@libero.it>
2018-02-20 15:07:01 +0100

    sommo

--- QuizAmoroso/QuizAmoroso/DataModel/Colori.cs
+++ QuizAmoroso/QuizAmoroso/DataModel/Colori.cs
@@ -11,10 +11,11 @@ namespace QuizAmoroso.DataModel
     {
         public static Color TemaApp = Color.FromHex("#072034");
         public static Color Button = Color.FromHex("#37637F");
+        public static Color Descrizione = Color.FromHex("#223A4D");
         public static Color ColoriSecondario = Color.FromHex("#37637F");
         public static Color coloreSottotitolo = Color.FromHex("#398A96");
         public static Color textColorTitolo = Color.FromHex("#BFC2E5");
-        public static Color coloreTerziario = Color.FromHex("#BFC2E5");
+        public static Color coloreTerziario = Color.FromHex("#BEDDF2");
 
     }
 }
