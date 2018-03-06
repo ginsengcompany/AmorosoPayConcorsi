4fff17de43aae171a5e4f0134615fb8070264149
FabriGinseng <antoniofabriziofiume@libero.it>
2018-03-06 17:04:01 +0100

    iniziata la pagina risultati

--- QuizAmoroso/QuizAmoroso/App.xaml.cs
+++ QuizAmoroso/QuizAmoroso/App.xaml.cs
@@ -19,7 +19,7 @@ namespace QuizAmoroso
         {
             InitializeComponent();
             // Indica la prima pagina che verrà visualizzata 
-            MainPage = new NavigationPage(new Login());
+            MainPage = new NavigationPage(new RisultatiQuiz(null));
         }
 
         protected override void OnStart()
